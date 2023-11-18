c802accb1f337efdb77790c46975b903dd05e325
Dmitry Grishin <grishinda10@ya.ru>
2023-11-18 21:07:22 +0500

    small rework of dirs, added passport model, intersection shader

--- Assets/scripts/PutStamp.cs
+++ Assets/scripts/PutStamp.cs
@@ -8,10 +8,9 @@ public class PutStamp : MonoBehaviour
 
     private void Update()
     {
-        RaycastHit hit;
         var ray = new Ray(objectPosition.position, -objectPosition.up);
         // Debug.DrawRay(ray.origin, ray.direction * rayLenght);
-        Physics.Raycast(ray, out hit, rayLenght);
+        Physics.Raycast(ray, out var hit, rayLenght);
         if (hit.collider.tag == "document")
         {
             var normalToRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
