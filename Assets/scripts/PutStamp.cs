d9b33e0e97fae786e9c9cc083d113850d40c4f8b
Dmitry Grishin <grishinda10@ya.ru>
2023-11-18 21:11:30 +0500

    script fix

--- Assets/scripts/PutStamp.cs
+++ Assets/scripts/PutStamp.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PutStamp : MonoBehaviour
@@ -5,16 +6,15 @@ public class PutStamp : MonoBehaviour
     public GameObject decalPrefab;
     public Transform objectPosition;
     public float rayLenght;
+    public string tagName;
 
     private void Update()
     {
         var ray = new Ray(objectPosition.position, -objectPosition.up);
         // Debug.DrawRay(ray.origin, ray.direction * rayLenght);
         Physics.Raycast(ray, out var hit, rayLenght);
-        if (hit.collider.tag == "document")
-        {
-            var normalToRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
-            Instantiate(decalPrefab, hit.point, normalToRotation);
-        }
+        if (hit.collider.tag != tagName) return;
+        var normalToRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        Instantiate(decalPrefab, hit.point, normalToRotation);
     }
 }
