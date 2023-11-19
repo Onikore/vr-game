154ef4e5a0b05efb422d2c576a45135d4481143e
Korepanov Danil <danil_korepanov_82@mail.ru>
2023-11-19 11:54:24 +0500

    жалюзи работают(плохо)

---
+++ Assets/scripts/ButtonPushCloseBlinds.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ButtonPushCloseBlinds : MonoBehaviour
+{
+    public Animator animator;
+
+    public string boolName = "Close";
+    private bool isClose = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<XRSimpleInteractable>().selectEntered.AddListener(x => BlindsClose());
+    }
+
+    public void BlindsClose()
+    {
+        isClose = true;
+        animator.SetBool(boolName, true);
+    }
+    public void CloseAnimationEnd()
+    {
+        isClose = false;
+        animator.SetBool(boolName, false);
+    }
+}
---
+++ Assets/scripts/ButtonPushOpenBlinds.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ButtonPushOpenBlinds : MonoBehaviour
+{
+    public Animator animator;
+
+    public string boolName = "Open";
+    private bool isOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<XRSimpleInteractable>().selectEntered.AddListener(x => BlindsOpen());
+    }
+
+    public void BlindsOpen()
+    {
+        isOpen = true;
+        animator.SetBool(boolName, true);
+
+    }
+    
+    public void OpenAnimationEnd()
+    {
+        isOpen = false;
+        animator.SetBool(boolName, false);
+    }
+}
